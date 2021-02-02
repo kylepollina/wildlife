05b60d4a192867f2e5c892fd994bfaec5f3470a1
kyle pollina <>
2021-02-02 10:43:39 -0600

    Add 'All birds' page

--- dbc-checklist/generate_checklist.py
+++ dbc-checklist/generate_checklist.py
@@ -38,33 +38,38 @@ html_end = """
 """
 
 
-def build_page(bird_data, output_file):
+def build_page(bird_data: pd.DataFrame, output_file: str, all_bird_page: bool = False):
+    """ Build the html page """
     html = html_start
 
-    season, abundance = output_file.split('/')
-    abundance = abundance.replace('.html', '')
+    if all_bird_page:
+        html = html.replace('../../styles.css', '../styles.css')
 
-    html += f'<u><h1>{season}</h1></u>'
+    else:
+        season, abundance = output_file.split('/')
+        abundance = abundance.replace('.html', '')
 
-    for s in ['winter', 'early-spring', 'late-spring', 'summer', 'post-breeding', 'early-fall', 'late-fall']:
-        html += "<a href='https://kylepollina.github.io/wildlife/dbc-checklist/{}/abundant.html'>".format(s)
-        html += "{}</a>".format(s.replace('-', ' '))
-        if s != 'late-fall':
-            html += " - "
+        html += f'<u><h1>{season}</h1></u>'
 
-    html += "<u><h3>{}</h3></u>".format(abundance.replace('-', ' '))
+        for s in ['winter', 'early-spring', 'late-spring', 'summer', 'post-breeding', 'early-fall', 'late-fall']:
+            html += "<a href='https://kylepollina.github.io/wildlife/dbc-checklist/{}/abundant.html'>".format(s)
+            html += "{}</a>".format(s.replace('-', ' '))
+            if s != 'late-fall':
+                html += " - "
 
-    for abd in ['abundant', 'common', 'fairly-common', 'uncommon', 'rare', 'extremely-rare']:
-        html += "<a href='https://kylepollina.github.io/wildlife/dbc-checklist/{}/{}.html'>".format(season, abd)
-        html += "{}</a>".format(abd.replace('-', ' '))
-        if abd != 'extremely-rare':
-            html += " - "
+        html += "<u><h3>{}</h3></u>".format(abundance.replace('-', ' '))
+
+        for abd in ['abundant', 'common', 'fairly-common', 'uncommon', 'rare', 'extremely-rare']:
+            html += "<a href='https://kylepollina.github.io/wildlife/dbc-checklist/{}/{}.html'>".format(season, abd)
+            html += "{}</a>".format(abd.replace('-', ' '))
+            if abd != 'extremely-rare':
+                html += " - "
 
     for bird, row in bird_data.iterrows():
         html += f"<h1>{bird}</h1>"
 
         if isinstance(row['img'], str):
-            html += '<img src="{}" />'.format(row['img']) if isinstance(row['img'], str) else ''
+            html += '<img src="{}" loading="lazy">'.format(row['img']) if isinstance(row['img'], str) else ''
         else:
             html += '<table width="100%">'
             html += '<col style="width:50%">'
@@ -77,14 +82,15 @@ def build_page(bird_data, output_file):
             html += '</thead>'
             html += '<tbody>'
             html += '<tr>'
-            html += '<td align="left"><img alt="" src="{}" /></td>'.format(row['female'])
-            html += '<td align="left"><img alt="" src="{}" /></td>'.format(row['male'])
+            html += '<td align="left"><img alt="" src="{}" loading="lazy"></td>'.format(row['female'])
+            html += '<td align="left"><img alt="" src="{}" loading="lazy"></td>'.format(row['male'])
             html += '</tr>'
             html += '</tbody>'
             html += '</table>'
             html += ''
 
-        html += row['recording_embed_link']
+        if not all_bird_page:
+            html += row['recording_embed_link']
 
         html += "<details><summary>More Information</summary>"
         html += "<ul>"
@@ -97,9 +103,9 @@ def build_page(bird_data, output_file):
         html += "<li><a href='{}'>Recordings</a></li>".format(row['allaboutbirds'].replace('/overview', '/sounds'))
         html += "</ul></ul>"
         html += "<h2>Range Map</h2>"
-        html += '<a href="{}"><img src="{}" style="width: 75%"/></a>'.format(row['allaboutbirds'].replace('/overview', '/maps-range'), row['migration'])
+        html += '<a href="{}"><img src="{}" style="width: 75%" loading="lazy"></a>'.format(row['allaboutbirds'].replace('/overview', '/maps-range'), row['migration'])
         html += "<h2>Fermilab Sighting Chart</h2>"
-        html += "<a href='https://www.fnal.gov/cgi-bin/ecology/wildlife/display?{formatted_name}'><img src='https://www.fnal.gov/cgi-bin/ecology/wildlife/bigbar?{formatted_name}'></a>".format(
+        html += '<a href="https://www.fnal.gov/cgi-bin/ecology/wildlife/display?{formatted_name}"><img src="https://www.fnal.gov/cgi-bin/ecology/wildlife/bigbar?{formatted_name}" loading="lazy"></a>'.format(
             formatted_name=bird.replace(' ', '+')
         )
         html += "</details>"
@@ -116,10 +122,12 @@ if __name__ == "__main__":
     df = pd.read_csv('bird_data.csv', index_col='bird')
 
     for season in ['winter', 'early spring', 'late spring', 'summer', 'post breeding', 'early fall', 'late fall']:
-        winter_birds = df.dropna(subset=[season])
+        winter_birds = df.dropna(subset=[season])  # Only drop NA rows in the current season
         build_page(winter_birds[winter_birds[season] == '1 - abundant'], '{}/abundant.html'.format(season.replace(' ', '-')))
         build_page(winter_birds[winter_birds[season] == '2 - common'], '{}/common.html'.format(season.replace(' ', '-')))
         build_page(winter_birds[winter_birds[season] == '3 - fairly common'], '{}/fairly-common.html'.format(season.replace(' ', '-')))
         build_page(winter_birds[winter_birds[season] == '4 - uncommon'], '{}/uncommon.html'.format(season.replace(' ', '-')))
         build_page(winter_birds[winter_birds[season] == '5 - rare'], '{}/rare.html'.format(season.replace(' ', '-')))
         build_page(winter_birds[winter_birds[season] == '6 - extremely rare'], '{}/extremely-rare.html'.format(season.replace(' ', '-')))
+
+    build_page(df, 'all.html', all_bird_page=True)
